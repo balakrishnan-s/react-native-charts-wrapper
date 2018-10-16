59d9b25230b7350e186cc403189676a7281347a9
root <balakrishnan.bca@gmail.com>
2018-10-16 16:25:43 +0800

    Initial commit1

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-/** @format */
+import BarChart from './lib/BarChart';
+import HorizontalBarChart from './lib/HorizontalBarChart';
+import BubbleChart from './lib/BubbleChart';
+import CandleStickChart from './lib/CandleStickChart';
+import LineChart from './lib/LineChart';
+import PieChart from './lib/PieChart';
+import RadarChart from './lib/RadarChart';
+import ScatterChart from './lib/ScatterChart';
+import CombinedChart from './lib/CombinedChart';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+module.exports = {
+  BarChart,
+  HorizontalBarChart,
+  BubbleChart,
+  CandleStickChart,
+  LineChart,
+  PieChart,
+  RadarChart,
+  ScatterChart,
+  CombinedChart
+};
